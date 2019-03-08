63a856cfedaf30ed1882f6b25346dc26da82c72d
John Fazioli <john.fazioli@namecheap.com>
2019-03-08 18:38:17 +0100

    wip

--- src/components/AmigaErrorBoundary/index.tsx
+++ src/components/AmigaErrorBoundary/index.tsx
@@ -1,9 +1,41 @@
-import React from "react";
+import * as React from "react";
+import {Component} from "react";
+import {createPortal} from "react-dom";
 
 // const ToggleContainer = styled.label``;
 
-export default class AmigaErrorBoundary extends React.Component {
+interface State {
+  error?: any;
+  errorInfo?: any;
+}
+
+function AmigaErrorBoundaryStyled() {
+  return <h1>Error</h1>;
+}
+
+export default class AmigaErrorBoundary extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      error: false,
+      errorInfo: false,
+    };
+  }
+
+  componentDidCatch() {
+    // Catch errors in any components below and re-render with error message
+    this.setState({
+      error: "ops",
+    });
+    // You can also log error messages to an error reporting service here
+  }
+
   render() {
-    return (<h1>Json</h1>);
+    if (this.state.error) {
+      return createPortal(<AmigaErrorBoundaryStyled/>, document.getElementsByTagName("body")[0]);
+    }
+
+    return this.props.children;
   }
 }
