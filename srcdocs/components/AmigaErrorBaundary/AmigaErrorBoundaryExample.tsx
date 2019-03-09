c7494dd6a27179561e2f4df7fe449ac438f509c9
John Fazioli <john.fazioli@namecheap.com>
2019-03-09 12:25:46 +0100

    update docs

--- srcdocs/components/AmigaErrorBaundary/AmigaErrorBoundaryExample.tsx
+++ srcdocs/components/AmigaErrorBaundary/AmigaErrorBoundaryExample.tsx
@@ -1,36 +1,17 @@
 import * as React from "react";
-import {useState} from "react";
+import { useState } from "react";
 
 import AmigaErrorBoundary from "../../../src/components/AmigaErrorBoundary";
-import {DocsExample} from "../../theme/theme";
-
-function Button() {
-  const [value, setValue] = useState(false);
-
-  function onClick() {
-    setValue(true);
-  }
-
-  if (value) {
-    throw new Error("I crashed!");
-  }
-
-  return <button onClick={onClick}>Create an exception</button>;
-}
+import { DocsExample } from "../../theme/theme";
 
 export function AmigaErrorBoundaryExample() {
-
   const [debug, setDebug] = useState(false);
 
   return (
-    <AmigaErrorBoundary debug={debug}>
-      <DocsExample>
-        <h1>AmigaErrorBoundary Example</h1>
-
-        <button onClick={() => setDebug(true)}>Debug</button>
-
-        <Button />
-      </DocsExample>
-    </AmigaErrorBoundary>
+    <DocsExample>
+      <AmigaErrorBoundary show={debug} error={"#fdc000"}/>
+      <h1>AmigaErrorBoundary Example</h1>
+      <button onClick={() => setDebug(true)}>Show</button>
+    </DocsExample>
   );
 }
