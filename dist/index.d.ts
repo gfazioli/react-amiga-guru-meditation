cba3deb516ac38e4c7626e8c7a2d3f46eb6bc1f6
John Fazioli <john.fazioli@namecheap.com>
2019-11-14 14:55:20 +0100

    update

---
+++ dist/index.d.ts
@@ -0,0 +1,1 @@
+export { default as AmigaErrorBoundary } from "./components/AmigaErrorBoundary";
---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var AmigaErrorBoundary_1 = require("./components/AmigaErrorBoundary");
+exports.AmigaErrorBoundary = AmigaErrorBoundary_1.default;
+//# sourceMappingURL=index.js.map
